dc1b2f40a89e8d0c010b8214b76013bdb6cd01c3
Sadhana.Sahu <sadhanasahu0710@gmail.com>
2020-08-04 01:13:13 +0530

    string_manipulation

---
+++ string_manipulation.py
@@ -0,0 +1,24 @@
+# String manipulation
+
+
+string = "Hello Everyone"
+
+print(string)
+
+#length of string
+print("length of string:",len(string))
+
+#print string in upper case
+print("upper case:",string.upper())
+
+#print string in lower case
+print("lower case:",string.lower())
+
+#access 1st char of string
+print("first char of string:",string[0])
+
+#access last char of string
+print("last char of string:",string[-1])
+
+#slicing 4th to last char
+print("4th to last char:",string[4:])
