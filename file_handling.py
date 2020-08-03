03a5597577fc40e5406cb9051513c51fe0064160
Sadhana.Sahu <sadhanasahu0710@gmail.com>
2020-08-04 01:45:42 +0530

    file handling

---
+++ file_handling.py
@@ -0,0 +1,13 @@
+# file handling
+
+file = open("new_file.txt","w+")
+
+file.write("This is our new file!\n")
+
+file.close()
+
+file = open("new_file.txt","a+")
+file.write("Add new line!")
+
+file = open("new_file.txt","r")
+print(file.read())
